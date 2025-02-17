a51c5f4b12f4fa7fb37dea1c27da5f46d06cda77
Pavel Gribanov <iamhab@gmail.com>
2025-02-17 15:19:49 +1000

    fix hw-4 task-2

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -15,14 +15,24 @@
 
   */
 
-  const minAge = 18;
-  const maxAge = 60;
-  let age
-  const checkedAge = "18";
+const minAge = 18;
+const maxAge = 60;
+const age = 61;
 
-  switch (age) {
-    case Number.isInteger(age):
-      age = checkedAge;
-      console.log(age)
-      break;
-  }
+if(typeof age !== "number") {
+  console.log("Incorrect data type");
+};
+
+if(age < minAge) {
+  console.log ("You don't have access cause your age is " + age + " It's less then " + minAge);
+} else if (age >= minAge && age < maxAge) {
+  console.log("Welcome  !");
+} else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel")
+} else {
+  console.log("Technical work")
+}
+
+
+
+
